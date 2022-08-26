2e0acbfc4c81993f8dbfb52ae7495079fc0e90a1
Project_Day_1 _of_100 <scosta.alexsander@gmail.com>
2022-08-25 21:54:06 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# 🚨 BMI CALCULATOR
+
+my_height = input("\nEnter your height in m: ")
+
+my_weight = input("\nEnter your weight in kg: ")
+
+bmi = (float(my_weight) / float(my_height) ** 2)
+
+print(bmi)
+
+if bmi < 18.6:
+    print("\nYou are Underweight. Go to the Doctor !\n\n")
+elif bmi <= 24.9:
+    print("\nYou're Healthy Weight. Good Job!\n\n")
+
+elif bmi <= 29.9:
+    print("You're Overweight, be careful. Do exercises\n\n")
+
+elif bmi > 29.9:
+    print("You're obesity. Go to the Doctor !\n\n")
