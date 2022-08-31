eb6189853684b11fe910593822f064a17696c349
Alexsandercosta <59259503+AlexsanderC-dev@users.noreply.github.com>
2022-08-31 19:55:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 # 🚨 BMI CALCULATOR
 
-my_height = input("\nEnter your height in m: ")
 
-my_weight = input("\nEnter your weight in kg: ")
+my_height = float(input("\nEnter your height in m: ")) # Use the " . " to separate the meter from the centimeter
 
-bmi = (float(my_weight) / float(my_height) ** 2)
+my_weight = float(input("\nEnter your weight in kg: "))
 
-print(round(bmi,2))
-
-if bmi < 18.6:
-    print("\nYou are Underweight. Go to the Doctor !\n\n")
-elif bmi <= 24.9:
-    print("\nYou're Healthy Weight. Good Job!\n\n")
-
-elif bmi <= 29.9:
-    print("You're Overweight, be careful. Do exercises\n\n")
-
-elif bmi > 29.9:
-    print("You're obesity. Go to the Doctor !\n\n")
+bmi = round(my_weight / my_height ** 2)
+if bmi < 18.5:
+    print(f"\nYou are Underweight, your bmi is {bmi}. Go to the Doctor !\n\n")
+elif bmi < 25:
+    print(f"\nYou're Healthy Weight, your bmi is {bmi}.  Good Job!\n\n")
+elif bmi < 30:
+    print(f"\nYou're Overweight, your bmi is {bmi}, be careful. Do exercises\n\n")
+elif bmi < 35:
+    print(f"\nYou're obesity, your bmi is {bmi} Go to the Doctor !\n\n")
+else:
+    print(f"\nYou're clinically obese, your bmi is {bmi}. It's very important that you see a doctor  ")
